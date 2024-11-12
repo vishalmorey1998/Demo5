4372def04804f142c9a9ca034335c6e3855717d5
vishalmorey1998 <vishalmorey2000@gmail.com>
2024-11-12 20:16:14 +0530

    Update JavaDemo5.java

--- javaDemo1/src/com/JavaDemo5.java
+++ javaDemo1/src/com/JavaDemo5.java
@@ -1,7 +1,12 @@
 package com;
 
 public class JavaDemo5 {
+	public void m1()
+	{
+        System.out.println("This is a M1 method .....");
 
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("This is a Github process");
 
